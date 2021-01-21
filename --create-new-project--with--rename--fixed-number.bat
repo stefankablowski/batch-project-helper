7053bfd74a17ad9f934736ab6e42fa65790ea4ca
stefankablowski <49285379+stefankablowski@users.noreply.github.com>
2021-01-21 15:02:36 +0100

    make fixed number work

--- --create-new-project--with--rename--fixed-number.bat
+++ --create-new-project--with--rename--fixed-number.bat
@@ -3,33 +3,47 @@ setlocal EnableDelayedExpansion
 set PATH_TO_TEMPLATE="__Templates"
 set "FIXED_LENGTH=4"
 set /p "P_NAME=enter your project name:"
+:: lowest project number to start with
 set lastFolder=0
-for /d %%d in (???_*) do (
+:: loop through all directories in current folder
+for /d %%d in (*_*) do (
     set folder="%%d"
-    setlocal EnableDelayedExpansion
+    :: split current folder in two parts 001_myfolder -> 001, myfolder
     for /f "tokens=1,2 delims=_" %%a in (!folder!) do set folder=%%a
-    if not defined folder set folder=0
-    if !folder! gtr !lastFolder! set lastFolder=!folder!
-)
 
-::remove leading zeroes
-for /f "tokens=* delims=0" %%N in ("!lastFolder!") do set "lastFolder=%%N"
-if not defined lastFolder set "lastFolder=0"
+    ::remove leading zeroes
+    for /f "tokens=* delims=0" %%M in ("!folder!") do set "folder=%%M"
+    if not defined folder set folder=0
+
+    ::check if folder number is numeric
+    SET "var="&for /f "delims=0123456789" %%i in ("!folder!") do set var=%%i
+    if not defined var (
+        if !folder! gtr !lastFolder! set lastFolder=!folder!
+    )
+
+)
+echo !lastfolder!
+pause
 
 ::calculate new folder number
 set /A nextFolder=!lastFolder!+1
 
+::add leading zeroes to folder number
 set /A endOfLoop=%FIXED_LENGTH%-1
 set "COMPARE_NUMBER=100"
 for /L %%Y in (1,1,%endOfLoop%) do (
     IF 1!nextFolder! LSS !COMPARE_NUMBER! SET nextFolder=0!nextFolder!
     set /A COMPARE_NUMBER=!COMPARE_NUMBER!*10
 )
-::add leading zeroes
-::IF 1%nextFolder% LSS 1000 SET nextFolder=0%nextFolder%
+
+::build project path
 set DESTINATION_PATH="%CD%\%nextFolder%_%P_NAME%"
+
+::copy files from template folder
 ROBOCOPY %PATH_TO_TEMPLATE% %DESTINATION_PATH% /E
 
+
+::recursively search through project directory and replace keywords
 @for /R "%DESTINATION_PATH%" %%I in (*.*) do (
     set CURRENT_NAME=%%I
     set NEW_NAME=!CURRENT_NAME:pnumber=%nextFolder%!
